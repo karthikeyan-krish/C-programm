4f9e93d3c05ce30d89ece09cc5b4e67cba616554
karthikeyan-krish <imkarthiknk@gmail.com>
2022-07-20 22:23:07 +0200

    LL 3rd commit

--- forward_linked_list.c
+++ forward_linked_list.c
@@ -8,10 +8,16 @@ typedef struct Node{
 }node;
 
 
+node* head;
+void Insert(int data);
+void Print();
+void Delete(int n);
 
-node* Insert(node *head, int X);
-void Print(node * head);
-
+//void Insert(int data, int n);
+//node* Insert(node *head, int X);
+//void Print(node * head);
+/* Code for forward inserting linkedList/*
+/*
 int main(){
     node *head = NULL;
     printf("How many elements you need?\n");
@@ -43,4 +49,92 @@ void Print(node * head){
     printf("%d\n",head->data);
     head = head->link;
     }
+}
+*/
+int main(){
+    head= NULL;
+    Insert(2);
+    Insert(3);
+    Insert(4);
+    Insert(5);
+    Print();
+    int n;
+    printf("Enter a position to delete\n");
+    scanf("%d",&n);
+    Delete(n);
+    Print();
+}
+
+/* insert function with n postion argument*/
+/*
+void Insert(int x, int n){
+    int i;
+    node* temp1= (node*)malloc(sizeof(node));
+    node* temp2= head;
+
+     temp1->data = x;
+     temp1->link = NULL;
+    if(n==1){
+        temp1->link = head;
+        head = temp1;
+        return;
+    }
+    for(i=0;i<n-2;i++)
+    {
+        temp2 = temp2->link;
+    }
+    temp1->link = temp2->link;
+    temp2->link = temp1;
+}
+*/
+void Insert(int x){
+    int i;
+    node* temp1= (node*)malloc(sizeof(node));
+    node* temp2= head;
+
+     temp1->data = x;
+     temp1->link = NULL;
+    // Checking if it is the first Node
+    if(head == NULL){
+        temp1->link = head;
+        head = temp1;
+        return;
+    }
+    //navigating to (N-1)th Node
+    while(temp2->link != NULL)
+    {
+        temp2 = temp2->link;
+    }
+    //linking (N-1)th Node and Nth Node
+    temp1->link = temp2->link;
+    temp2->link = temp1;
+}
+
+void Print(){
+    node* temp= head;
+    while(temp != NULL){
+        printf("%d",temp->data);
+        temp = temp->link;
+    }
+    printf("\n");
+}
+
+void Delete(int n)
+{
+    node* temp1 = head;
+    if(n == 1)
+    {
+        head = temp1->link; // head now points to the second node so we can delete first node
+        free(temp1);
+        return;
+    }
+    int i;
+    for(i=0;i<n-2;i++)
+    {
+        temp1 = temp1->link;
+    }
+    //temp1 points to (n-1)th node
+    node* temp2 = temp1->link; // nth Node to be deleted
+    temp1->link = temp2->link; //linking (N-1)th Node and (n+1)th Node
+    free(temp2); //  deleting Nth Node
 }
